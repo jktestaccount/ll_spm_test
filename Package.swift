362e9f8f0985b09921f559614802fd22c9a35398
JT <jt@JK-MacBook-Pro.local>
2025-01-02 17:34:32 +0200

    changes

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        // More notes added
+        // More notes added v2
         .binaryTarget(
             name: "LocusLabsSDK",
             path: "./Sources/LocusLabsSDK.xcframework"),
